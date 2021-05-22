012878974c46b09e9c621ee0a6f568e168a16659
Iryna Volosozharova <iradevis@gmail.com>
2021-05-22 22:34:50 +0300

    added pnotify plugin

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './sass/main.scss';
+import '@pnotify/core/dist/BrightTheme.css';
+import { error } from '@pnotify/core';
 import { debounce } from 'lodash';
 import listMarkUp from './templates/list-template.hbs';
 import cardMarkup from './templates/card-template.hbs';
@@ -13,8 +15,20 @@ refs.input.addEventListener('input', debounce(onSearch, 500))
 function onSearch(e) {
     const searchQuery = e.target.value
     fetchCountry(searchQuery)
-        .then(renderCountryCard)
-        .catch(error => error)
+        .then(countries => {
+            if (countries.length === 1) {
+                renderCountryCard(countries)
+            }
+            if (countries.length <= 10 && countries.length > 1) {
+        renderCountryList(countries)
+            }
+            if (countries.length > 10) {
+                error({
+  text: "Too many matches found. Please enter a more specific query!"
+})
+            }
+        })
+            .catch(error => error)
 }
 
 function fetchCountry(searchQuery) {
@@ -29,3 +43,7 @@ function renderCountryCard(country) {
 function renderCountryList(countries) {
     refs.searchResult.innerHTML = listMarkUp(countries);
 }
+
+// const error = error({
+//   text: "Too many matches found. Please enter a more specific query!"
+// });
